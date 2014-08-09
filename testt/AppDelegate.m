fea1a8e936e35fde43f213529830a4b7eae9b9c6
panda125125 <wangdengpeng@wangdeiMac.local>
2014-08-10 01:45:14 +0800

    34444

--- testt/AppDelegate.m
+++ testt/AppDelegate.m
@@ -3,7 +3,7 @@
 //  testt
 //
 //  Created by 王 登鹏 on 14-8-10.
-//  Copyright (c) 2014年 王 登鹏. All rights reserved.
+//  Copyright (c) 2014年 王 登鹏xxxx. All rights reserved.
 //
 
 #import "AppDelegate.h"
@@ -16,12 +16,13 @@
             
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
     // Override point for customization after application launch.
     return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types ofmporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
